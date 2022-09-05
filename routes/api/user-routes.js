b00cc41708e043cb3f8faf05108ee140a80201f8
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-09-05 11:14:26 -0500

    updated post route for login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,6 +45,24 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+    router.post('/login', (req, res) => {
+        User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(dbUserData => {
+            if (!dbUserData) {
+                res.status(400).json({ message: 'No user with that email address!' });
+                return;
+            }
+            const validPassword = dbUserData.checkPassword(req.body.password);
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect Password' });
+                return;
+            }
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        })
+    });
 });
 
 router.put('/:id', (req, res) => {
